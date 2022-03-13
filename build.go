637e3f0666e27d3248051328cd4e91d89b2e4ef1
Norwin <noerw@noreply.gitea.io>
2022-03-13 08:53:18 +0800

    Fix CI: run make fmt (#469)
    Co-authored-by: Norwin <git@nroo.de>
    Reviewed-on: https://gitea.com/gitea/tea/pulls/469
    Reviewed-by: 6543 <6543@obermui.de>
    Co-authored-by: Norwin <noerw@noreply.gitea.io>
    Co-committed-by: Norwin <noerw@noreply.gitea.io>

--- build.go
+++ build.go
@@ -1,7 +1,8 @@
 // Copyright 2020 The Gitea Authors. All rights reserved.
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
-//+build vendor
+//go:build vendor
+// +build vendor
 
 package main
 
